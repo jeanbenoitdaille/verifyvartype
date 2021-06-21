397adacee9ba4294de60ff3bd3f1d466913b6fc4
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-06-22 00:04:31 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+prenom = "Pierre"
+     
+if type(prenom) == str:
+        print("La variable est une chaîne de caractères")
+     
+prenom = 0
+     
+if isinstance(prenom, str):
+        print("La variable est une chaîne de caractères")
